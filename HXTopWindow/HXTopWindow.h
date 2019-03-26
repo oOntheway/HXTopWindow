0098d4cd95561e6080a9113557813db58468beef
oOntheway <dl_xq@sina.com>
2019-03-26 13:35:40 +0800

    Update HXTopWindow.h

--- HXTopWindow/HXTopWindow.h
+++ HXTopWindow/HXTopWindow.h
@@ -1,9 +1,7 @@
 //
 //  HXTopWindow.h
-//  百思不得姐
 //
-//  Created by HuaXin on 16/7/21.
-//  Copyright © 2016年 HuaXin. All rights reserved.
+//  Created on 16/7/21.
 //  点击状态栏, 让scrollView滚动到顶部
 
 #import <UIKit/UIKit.h>
